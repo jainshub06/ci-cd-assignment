1f2fe1d382d21eed5f29e93e401c675f0a6f3512
Shubham Jain <57320634+jainshub06@users.noreply.github.com>
2024-08-02 02:41:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hello World!!!")
+
+import time
+ 
+try:
+    num = 11
+    while True: 
+        if num % 2 == 0:
+            break
+        print(num)
+        num = num + 2
+        time.sleep(1)  # Using to slow the while loop by one second 🙂
+except KeyboardInterrupt:
+    pass
+ 
+print("Continuing with the program")
