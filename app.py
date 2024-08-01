88ab41c46f6e64ca9e1f282f59724bf5a86a81d8
Shubham Jain <57320634+jainshub06@users.noreply.github.com>
2024-08-02 02:47:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import time 
-try:
-    num = 11
-    while True: 
-        if num % 2 == 0:
-            break
-        print(num)
-        num = num + 2
-        time.sleep(1)  # Using to slow the while loop by one second 🙂
-except KeyboardInterrupt:
-    pass
-print("Continuing with the program")
+num = 11
+time.sleep(20)
+print(num)
